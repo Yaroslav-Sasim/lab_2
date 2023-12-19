9b6a5b0f5efd26eb4512bffed4932c1774755ecb
Yaroslav-Sasim <62548404+Yaroslav-Sasim@users.noreply.github.com>
2023-12-19 15:27:08 +0300

    Concepts main released

--- lib/class_hierarchy/Shape3D.dart
+++ lib/class_hierarchy/Shape3D.dart
@@ -1,32 +1,9 @@
-// Абстрактный класс Shape3D
+// Класс Shape3D с функциями различных типов параметров
 abstract class Shape3D {
   double calculateVolume();
-}
 
-// Класс Cube, наследник Shape3D
-class Cube extends Shape3D {
-  double side;
-
-  // Конструктор класса Cube
-  Cube(this.side);
-
-  // Реализация метода calculateVolume для куба
-  @override
-  double calculateVolume() {
-    return side * side * side;
-  }
-}
-
-// Класс Sphere, наследник Shape3D
-class Sphere extends Shape3D {
-  double radius;
-
-  // Конструктор класса Sphere
-  Sphere(this.radius);
-
-  // Реализация метода calculateVolume для сферы
-  @override
-  double calculateVolume() {
-    return (4 / 3) * 3.14159 * radius * radius * radius;
+  // Функция с различными типами параметров
+  void functionWithDifferentParameters(int a, double b, String c) {
+    // Ваш код здесь
   }
 }
